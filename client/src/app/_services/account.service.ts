9f1f410cdb84507d1629a7801d7c1bec714a60fa
dukanacv <dukanacvla@gmail.com>
2022-03-14 19:47:05 +0100

    persist the login

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, ReplaySubject } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({//service are injected in other comp in app; will stay initialized UNTIL APP IS CLOSED
   providedIn: 'root'//provided in app.module.ts
@@ -7,10 +9,30 @@ import { Injectable } from '@angular/core';
 //will be used to make req to API
 export class AccountService {
   baseUrl = "https://localhost:5001/api/"
+  private currentUserSource = new ReplaySubject<User | null>(1)//kinda like buffer object; (1) => size of buffer(user object for current user)
+  currentUser$ = this.currentUserSource.asObservable()
 
   constructor(private htpp: HttpClient) { }
 
   login(model: any) {
-    return this.htpp.post(this.baseUrl + "account/login", model)//model contains username and password
+    return this.htpp
+      .post<User>(this.baseUrl + "account/login", model).pipe(
+        map((response: User) => {
+          const user = response
+          if (user) {
+            localStorage.setItem("user", JSON.stringify(user))
+            this.currentUserSource.next(user)
+          }
+        })
+      )//model contains username and password
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user)
+  }
+
+  logout() {
+    localStorage.removeItem("user")
+    this.currentUserSource.next(null)
   }
 }
