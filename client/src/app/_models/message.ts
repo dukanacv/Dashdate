4df1566a1413346d3fce25cff0f8714237db5d19
dukanacv <dukanacvla@gmail.com>
2022-06-08 18:23:01 +0200

    added messages serv, comp, html view

---
+++ client/src/app/_models/message.ts
@@ -0,0 +1,14 @@
+export interface Message {
+    id: number;
+    senderId: number;
+    senderUsername: string;
+    senderPhotoUrl: string;
+    receiverId: number;
+    receiverUsername: string;
+    receiverPhotoUrl: string;
+    content: string;
+    dateRead?: Date;
+    dateSent: Date;
+}
+
+
